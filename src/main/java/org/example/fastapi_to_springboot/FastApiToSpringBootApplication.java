0a5983b8df54f5654fff6e93ef9751d0bf3c4421
mena <tsikiniainatsilavina@gmail.com>
2026-03-13 11:02:28 +0300

    Initial commit

---
+++ src/main/java/org/example/fastapi_to_springboot/FastApiToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.fastapi_to_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastApiToSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastApiToSpringBootApplication.class, args);
+    }
+
+}
