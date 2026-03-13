cefd11a61f876508887adf33649ec00af793b2a0
mena <tsikiniainatsilavina@gmail.com>
2026-03-13 11:10:23 +0300

    ajout des methode dans BookingController'

--- src/main/java/org/example/fastapi_to_springboot/controller/BookingController.java
+++ src/main/java/org/example/fastapi_to_springboot/controller/BookingController.java
@@ -1,4 +1,40 @@
 package org.example.fastapi_to_springboot.controller;
 
+import java.time.LocalDate;
+
 public class BookingController {
+
+        private String clientName;
+        private String phone;
+        private String email;
+        private int roomNumber;
+        private String roomDescription;
+        private LocalDate bookingDate;
+
+        public BookingController() {}
+
+        public BookingController(String clientName, String phone, String email,
+                       int roomNumber, String roomDescription, LocalDate bookingDate) {
+            this.clientName = clientName;
+            this.phone = phone;
+            this.email = email;
+            this.roomNumber = roomNumber;
+            this.roomDescription = roomDescription;
+            this.bookingDate = bookingDate;
+        }
+
+        public String getClientName() { return clientName; }
+        public String getPhone() { return phone; }
+        public String getEmail() { return email; }
+        public int getRoomNumber() { return roomNumber; }
+        public String getRoomDescription() { return roomDescription; }
+        public LocalDate getBookingDate() { return bookingDate; }
+
+        public void setClientName(String clientName) { this.clientName = clientName; }
+        public void setPhone(String phone) { this.phone = phone; }
+        public void setEmail(String email) { this.email = email; }
+        public void setRoomNumber(int roomNumber) { this.roomNumber = roomNumber; }
+        public void setRoomDescription(String roomDescription) { this.roomDescription = roomDescription; }
+        public void setBookingDate(LocalDate bookingDate) { this.bookingDate = bookingDate; }
+
 }
